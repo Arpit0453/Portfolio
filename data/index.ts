b9d4c537f9b98e11805fd9e57d0dde54c36c148c
Arpit Khatri <123655325+Arpit0453@users.noreply.github.com>
2025-07-17 14:00:13 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -75,7 +75,7 @@ export const projects = [
     des: "CortexGrid empowers intelligence through neural learning, perception, reasoning, prediction, adaptation, and synthetic cognitive evolution.",
     img: "/p1.svg",
     iconLists: ["/re.svg", "/tail.svg", "/three.svg", "/fm.svg"],
-    link: "https://cortex-grid-p8l2zz9j7-arpit0453s-projects.vercel.app/",
+    link: "https://cortex-grid-yxjs.vercel.app/",
   },
   {
     id: 2,
